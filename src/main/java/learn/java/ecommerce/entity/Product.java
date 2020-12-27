036daec8c636cfe26e64d35c237c71f226bf94ef
mu.himel <mu.himel@gmail.com>
2020-12-27 22:44:20 +0600

    'product-repository'

---
+++ src/main/java/learn/java/ecommerce/entity/Product.java
@@ -0,0 +1,53 @@
+package learn.java.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_category_id",nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "units_in_stock")
+    private Integer unitsInStock;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Date updatedAt;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+
+insert into product_categories(category_name) values('Book')
+insert into products(name,description,sku,product_category_id) VALUES('Test','Description','123',1);
